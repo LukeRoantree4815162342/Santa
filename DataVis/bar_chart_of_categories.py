494a31aaa3906bf14dd44908ff46116214485dda
LUKEPC\Luke <allhailluke@roantree.com>
2017-01-24 00:19:26 +0000

    initial data vis

---
+++ DataVis/bar_chart_of_categories.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 23:25:19 2017
+
+@author: Luke
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+script_dir = os.path.dirname(__file__)
+gifts_csv_location = os.path.join(script_dir, "../giftsdata.csv")
+gifts = open(gifts_csv_location, 'r')
+gifts = gifts.read()
+gifts = gifts.split('\n')
+
+categories = []
+counts = {}
+def categorise(gift):
+    gift = gift.split('_')
+    gift = gift[0]
+    if gift in categories:
+        counts[gift]+=1
+    else:
+        categories.append(gift)
+        counts[gift]=1
+
+for i in range(1,len(gifts)):
+    categorise(gifts[i])
+#   print counts
+
+temp = []
+for i in categories:
+    temp.append(counts[i])
+x = plt.bar(np.arange(len(categories)), temp, 0.35, label='categories')
+plt.xlabel('categories')
+plt.ylabel('counts')
+plt.xticks(np.arange(len(categories)) + 0.35 / 2, categories)
+plt.show()
+    
+    
