41c01d604a69bc4dd5472903fcc4ddb67b50eb23
LUKEPC\Luke <allhailluke@roantree.com>
2017-01-25 03:34:02 +0000

    removed commented code

--- Submissions/submission_one_random.py
+++ Submissions/submission_one_random.py
@@ -6,11 +6,7 @@ Created on Tue Jan 24 23:45:30 2017
 """
 
 import numpy as np
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import random
 import csv
-#import seaborn as sb
 import os
 
 script_dir = os.path.dirname(__file__)
@@ -18,35 +14,16 @@ gifts_csv_location = os.path.join(script_dir, "../giftsdata.csv")
 gifts = open(gifts_csv_location, 'r')
 gifts = gifts.read()
 gifts = gifts.split('\n')
-#print gifts
+
 submission_one_csv = open("giftsdata.csv", 'wt')
 np.random.shuffle(gifts)
-#print gifts
-#submission_one_csv.write()
 
-#print gifts[3+7]
-#print gifts[1]
-#
 writer = csv.writer(submission_one_csv, quoting=csv.QUOTE_NONNUMERIC)
 for i in range(len(gifts)/10):
     row = []
     row.append('')
     for j in range(0,9):
         row[0] = ''.join([row[0],gifts[i+j], ' '])
-#        print row[0]
-	#print gifts[i+j]
-#    row[0] = ' '.join(row[0])
-#    #print row
-#    row = tuple(row)
     writer.writerow(row)
 submission_one_csv.close()
-#
-
-#print open("giftsdata.csv", 'rt').read()
-#submission_one_csv = ''
-#for i in range(len(gifts)/10):
-#    for j in range(0,9):
-#        submission_one_csv += gifts[i+j] + ' '
-#    submission_one_csv += ','
-#submission_one_csv.close()
 
