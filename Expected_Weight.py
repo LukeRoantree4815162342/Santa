7a7a5c3b6e7393f728caa9f451b706d0d2d84576
LUKEPC\Luke <allhailluke@roantree.com>
2017-01-26 22:59:35 +0000

    updated to use profile class

--- Expected_Weight.py
+++ Expected_Weight.py
@@ -7,17 +7,28 @@ Created on Thu Jan 26 21:56:08 2017
 
 import TryEval
 import numpy as np
+import global_vars as gv
 
-example_profile = 'ball#2+bike#2+gloves#1'
+
+#
+#def get_weight_of_profile_once(profile):
+#    gifts_grouped = profile.split('+')
+#    gifts = []
+#    for gift_type in gifts_grouped:
+#        gifts_of_type = [gift_type.split('#')[0]]*int(gift_type.split('#')[1])
+#        gifts.extend(gifts_of_type)
+#    weight_once = TryEval.try_eval_bag_array(gifts)
+#    return weight_once
+#    
+
 
 def get_weight_of_profile_once(profile):
-    gifts_grouped = profile.split('+')
-    gifts = []
-    for gift_type in gifts_grouped:
-        gifts_of_type = [gift_type.split('#')[0]]*int(gift_type.split('#')[1])
-        gifts.extend(gifts_of_type)
-    weight_once = TryEval.try_eval_bag_array(gifts)
-    return weight_once
+    running_total_score = 0
+    for present in gv.presents_in_alpha_order:
+        present_count_in_profile = profile.presents[present]
+        for i in range(present_count_in_profile):
+            running_total_score += TryEval.gen_prob_wt(present)
+    return running_total_score
     
 
 def average_weight_over_x_simulations(x, profile):
