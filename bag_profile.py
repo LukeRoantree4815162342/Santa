5f6542a1cd46579e41b7b5e05cdcc4759ac49a32
droantree <donal@roantree.com>
2017-01-26 23:10:13 +0000

    Added way to add presents to Profile object.

--- bag_profile.py
+++ bag_profile.py
@@ -4,17 +4,8 @@ simply keeps track of how many of each present is in the bag.  Each profile has 
 e.g. ball#1+bike#3+gloves#2
 The present names are always in alpha order in the key so two profiles with the same number of each
 present will always have the same key.
-The present spellings are (exactly):
 
-ball
-bike
-blocks
-book
-coal
-doll
-gloves
-horse
-train
+Use the present variables in global_vars when adding to Profile objects
 '''
 
 import collections as cl
@@ -35,3 +26,15 @@ class Profile:
                 pkey_empty = False
                 pkey += present + "#" + str(present_count_in_profile)
         return pkey
+
+    def addPresentCount(self, present, count):
+        c = cl.Counter()
+        c[present] = count
+        self.presents = self.presents + c
+
+#test
+#p = Profile()
+#p.addPresentCount(gv.horse, 3)
+#p.addPresentCount(gv.book, 1)
+#print p.presents
+#print p.key()
