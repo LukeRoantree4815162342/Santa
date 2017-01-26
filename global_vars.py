15c6bafcacbda50bf88d5ff230fe49dec63e3951
droantree <donal@roantree.com>
2017-01-26 22:41:33 +0000

    This global_vars should generally be included in other files.

---
+++ global_vars.py
@@ -0,0 +1,16 @@
+#Global variables
+
+ball = "ball"
+bike = "bike"
+blocks = "blocks"
+book = "book"
+coal = "coal"
+doll = "doll"
+gloves = "gloves"
+horse = "horse"
+train = "train"
+
+presents_in_alpha_order = [ball, bike, blocks, book, coal, doll, gloves, horse, train]
+
+presents_in_size_order = [coal, bike, blocks, train, horse, doll, book, ball, gloves]
+#Thanks Christopher for that handy_numbers file - that's where I got the size order
