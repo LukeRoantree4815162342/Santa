e10c235b7160773089fd0a279d12cd9f3eaabcc8
LUKEPC\Luke <allhailluke@roantree.com>
2017-01-27 00:17:30 +0000

    updates before bed

--- submission_one_firstfit.py
+++ submission_one_firstfit.py
@@ -8,6 +8,9 @@ Created on Tue Jan 24 23:45:30 2017
 import numpy as np
 import csv
 import os
+import global_vars as gv
+import gift_ids as gi
+import bag_profile as bp
 
 script_dir = os.path.dirname(__file__)
 gifts_csv_location = os.path.join(script_dir, "giftsdata.csv")
@@ -15,7 +18,7 @@ gifts = open(gifts_csv_location, 'r')
 gifts = gifts.read()
 gifts = gifts.split('\n')
 
-submission_one_csv = open("possible_submission_bagsof7b.csv", 'wb')
+submission_one_csv = open("sub3.csv", 'wb')
 np.random.shuffle(gifts)
 
 writer = csv.writer(submission_one_csv, quoting=csv.QUOTE_NONNUMERIC)
@@ -26,11 +29,20 @@ bagsize = 7
 for i in range(len(gifts)/bagsize -25): #25 is specific to bagsize of 7; makes it 25 over limit
     row = []
     row.append('')
-    for j in range(0,9):
-        if j<9:
+    for j in range(0,6):
+        if j<6:
             row[0] = ''.join([row[0],gifts[bagsize*i+j], ' '])
         else:
             row[0] = ''.join([row[0],gifts[bagsize*i+j]])
     writer.writerow(row)
 submission_one_csv.close()
+#
+#
+#for i in range(len(dad_array)): #25 is specific to bagsize of 7; makes it 25 over limit
+#    row = []
+#    row.append('')
+#    for j in range(0,9):
+#        row[0] = ''.join([row[0], dad_array])
+#    writer.writerow(row)
+#submission_one_csv.close()
 
