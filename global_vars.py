24dfba5c4e676457a61bcf9114171be471785726
LUKEPC\Luke <allhailluke@roantree.com>
2017-01-27 20:02:36 +0000

    added average weights to global_vars

--- global_vars.py
+++ global_vars.py
@@ -15,4 +15,16 @@ presents_in_alpha_order = [ball, bike, blocks, book, coal, doll, gloves, horse, 
 presents_in_size_order = [coal, bike, blocks, train, horse, doll, book, ball, gloves]
 #Thanks Christopher for that handy_numbers file - that's where I got the size order
 
+average_weights = {}
+average_weights[ball] = 20.072
+average_weights[bike] = 5.0029
+average_weights[blocks] = 2.0003
+average_weights[book] = 11.668
+average_weights[coal] = 23.472
+average_weights[doll] = 1.4022
+average_weights[gloves] = 10.033
+average_weights[horse] = 4.9998
+average_weights[train] = 2.0015
+
+
 default_simulations = 1000
