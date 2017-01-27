2aed0327f94b666a7113ffbf2bc21c2ea425936c
droantree <donal@roantree.com>
2017-01-27 00:44:27 +0000

    a class to cache the expected weights of profiles so we're not re-calculating unnecessarily

---
+++ profile_expected_weights.py
@@ -0,0 +1,15 @@
+import bag_profile as bp
+import Expected_Weight as ew
+import global_vars as gv
+
+
+class ProfileExpectedWeights:
+    def __init__(self):
+        self.weights_cache = {}
+
+    def get_profile_weight(self, profile):
+        pkey = profile.key
+        if not (pkey in self.weights_cache):
+            weight = ew.average_weight_over_x_simulations(gv.default_simulations, profile)
+            self.weights_cache[pkey] = weight
+        return self.weights_cache[pkey]
