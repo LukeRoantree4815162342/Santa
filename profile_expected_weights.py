722ec63c4685cdc130dadb4deecd5108c8433df8
droantree <donal@roantree.com>
2017-01-27 01:03:51 +0000

    added test to ProfileExpectedWeights (caching class)

--- profile_expected_weights.py
+++ profile_expected_weights.py
@@ -8,8 +8,25 @@ class ProfileExpectedWeights:
         self.weights_cache = {}
 
     def get_profile_weight(self, profile):
-        pkey = profile.key
+        pkey = profile.key()
         if not (pkey in self.weights_cache):
             weight = ew.average_weight_over_x_simulations(gv.default_simulations, profile)
             self.weights_cache[pkey] = weight
         return self.weights_cache[pkey]
+
+    def cache_count(self):
+        return len(self.weights_cache)
+
+#test
+#p = bp.Profile()
+#p.addPresentCount(gv.horse, 3)
+#p.addPresentCount(gv.book, 1)
+#pew = ProfileExpectedWeights()
+#print "Cache size: " + str(pew.cache_count())
+#weight = pew.get_profile_weight(p)
+#print "Cache size: " + str(pew.cache_count())
+#weight = pew.get_profile_weight(p)
+#print "Cache size: " + str(pew.cache_count())
+#p.addPresentCount(gv.book, 1)
+#weight = pew.get_profile_weight(p)
+#print "Cache size: " + str(pew.cache_count())
